2878b771afea5543a0aa4f7a4f9a36c58d62f9fc
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-14 02:51:27 +0000

    datatypes

---
+++ Js-practice/01-veriables.js
@@ -0,0 +1,11 @@
+
+const accountId = 12222
+let  accountUsername = "jaydeepkushwaha@gmail.com"
+var  accountPassword = "jayadeep200"
+// accountdetail = "etc"
+
+// accountId =154545    // *note  we can not change the value which is assignet in const veriable
+accountUsername="raman@gmail.com"
+accountPassword=121211
+
+console.table([accountId ,accountUsername,accountPassword])
---
+++ Js-practice/02-datatypes.js
@@ -0,0 +1,12 @@
+"use strict" // use all code and syntex  as a new version 
+/* code readbility is important */
+
+// number => 2 to the power 53
+// bigInt => big numbers
+// Strings => always write in the  double course " jeydeep"
+// boolean => always give true/false output
+// null => stand alone value
+// undefined 
+// Symbol => unique
+
+//object
