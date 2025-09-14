ab6c4b0e4b8513d71e669a2509fa17a340751bbf
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-14 04:01:35 +0000

    operation

---
+++ Js-practice/04-operation.js
@@ -0,0 +1,27 @@
+// let A = 10;
+// let B = 20;
+// console.log(A + B);
+// console.log(A - B);
+// console.log(A / B);
+// console.log(A % B);
+// console.log(A * B);
+
+// console.log("1"+2+3); // if the first operation is string then all conciderd as a string
+// console.log(1+2+"3"); // always do first operations
+
+// incriment
+
+let counter = 100
+console.log(counter);
+++counter // pre incriment
+console.log(counter);
+counter++ // post incriment
+console.log(counter);
+
+
+--counter // pre dicriment
+console.log(counter);
+counter-- // post dicriment
+console.log(counter);
+
+
