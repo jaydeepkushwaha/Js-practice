eb0a34d3f48809d1de1936c0c72f9344fb55e7b2
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-14 03:15:41 +0000

    conversaction

---
+++ Js-practice/03-conversection.js
@@ -0,0 +1,22 @@
+// let scope = null
+
+// console.log(typeof scope);
+// console.log(typeof (scope));
+
+// let typecast = Number(scope);
+// console.log(typecast);
+
+
+// if we convert "33"=> 33 it become isily
+// but "121hdg" this is not convert but give NaN  (not a number)
+//null give 0
+// true always give 1
+//false always give 0
+ 
+
+let convertString = ""
+let isBoolean = Boolean (convertString)
+console.log(isBoolean);
+
+// we also typecast string to boolean easily
+// empty string give false value in boolean datatype
