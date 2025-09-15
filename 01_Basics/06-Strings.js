b68d3ab00b0f29a5cc8604dc19ffe1d30264c868
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-15 15:58:53 +0000

    Strings operations

---
+++ 01_Basics/06-Strings.js
@@ -0,0 +1,26 @@
+const name = "jaydeep kushwaha"
+// const myAge = 20
+
+// console.log(name + myAge + 2004);
+
+// 
+
+// console.log(name[5]);
+// console.log(name);
+// console.log(name.trim());
+// console.log(name.replace( 'kushwaha', 'kumar'))
+// console.log(name.replace( 'kumar', 'kushwaha'))
+// console.log(name.charAt(5));
+// console.log(name.indexOf("k"));
+
+const url = "https://www.jaydeep-kushwaha.com"
+console.log(url.replace( "-","_"));
+console.log(url.includes('kushwahay'));
+
+console.log(name.split(" "));
+
+
+
+
+
+
