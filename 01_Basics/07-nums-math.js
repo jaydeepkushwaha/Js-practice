b07f1f5079edcf6384fa4f2a27dde5b0b4e79059
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-15 16:19:11 +0000

    numbers operation

---
+++ 01_Basics/07-nums-math.js
@@ -0,0 +1,16 @@
+// // const score = 100
+
+// // console.log(score);
+
+// const hundreds = new Number(100.000)
+// console.log(hundreds.toString());
+
+// // console.log(hundreds.toFixed(2)); // always fixed  two zeros after decimal in the number  specially used in e-commerse website
+
+// // console.log(hundreds.toPrecision(4)); 
+
+// const newNumbers = 100000000
+
+// console.log(newNumbers.toLocaleString("en-IN")); // use for convert number in india standerds like  100000000 => 10,00,00,000
+
+// // +++++++++++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++
