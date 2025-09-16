ef49dc38fae5f6f41a77d2301e879d4fc5fcb2bd
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-09-16 16:14:19 +0000

    new learning

---
+++ 01_Basics/08-Date.js
@@ -0,0 +1,9 @@
+// let date = new Date ()
+
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+
+let myCreatedDate =new Date()
+console.log(myCreatedDate.toLocaleString());
---
+++ 02_Basics/01-Array.js
@@ -0,0 +1,31 @@
+const myArr = [0,1,2,3,4,5]
+
+console.log(myArr);
+
+
+// array Methods
+
+// console.log(myArr.push(7));// array menew value add karne ke liye push ka use karte hai
+// console.log(myArr.pop());// recent add ki hui value ko array se hata deta hai
+
+// myArr.unshift(5) // array ke start me value add karta hai
+// myArr.shift() // recent add ki hui value ko delete kar deta hai
+
+
+// console.log(myArr.includes(5)); // ye batata hai ki  puuchi hui value array me exist karti hai ki nahi
+// console.log(myArr.indexOf(9));// ye element ka index batata hai ki kis index pe kon sa element hai  (agar value exist nahi karti to  -1 show karta hai)
+
+// const newArr = myArr.join() // (join()) array ko string me convert kar deta hai
+
+//++++++++++++++++++++++++++++++++++++ Slice / Splice ++++++++++++++++++++++++++++++++++++++++++
+
+console.log(myArr.slice (0,3))
+console.log(myArr);
+
+console.log(myArr.splice(3,5));
+
+
+
+
+console.log(myArr);
+
