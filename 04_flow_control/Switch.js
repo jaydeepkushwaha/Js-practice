f2086db82b07d1b5182f89dcff94453f9a40749c
Jaydeep Kushwaha <157147691+jaydeepkushwaha@users.noreply.github.com>
2025-10-03 16:18:48 +0000

    if-else , Switch , truthy values

---
+++ 04_flow_control/Switch.js
@@ -0,0 +1,44 @@
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("Aprail");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    case 9:
+        console.log("september");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("nevember");
+        break;
+    case 12:
+        console.log("december");
+        break;
+
+    default:
+        console.log("this month is not existing");
+        break;
+}
